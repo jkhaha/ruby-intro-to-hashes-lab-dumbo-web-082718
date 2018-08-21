2e681a5a24c1394dc350254875bc8695e88dc537
Jackie Ha <jha2@wellesley.edu>
2018-08-21 02:39:57 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,7 +3,7 @@ def new_hash
 end
 
 def actor
-  actor = {:name => "Dwayne the Rock Johnson"}
+  actor = {:name => "Dwayne The Rock Johnson"}
 end
 
 def monopoly
@@ -42,10 +42,10 @@ def monopoly_with_fourth_tier
 	monopoly = {:railroads => {
 	  :pieces => 4,
 	  :names => {
-	    :reading_railroad => {"mortgage value" => "$100"}, 
-	    :pennsylvania_railroad => {"mortgage value" => "$200"},
-	    :b_and_o_railroad => {"mortgage value" => "$400"}, 
-	    :shortline => {"mortgage value" => "$800"
+	    :reading_railroad => {"mortgage_value" => "$100"}, 
+	    :pennsylvania_railroad => {"mortgage_value" => "$200"},
+	    :b_and_o_railroad => {"mortgage_value" => "$400"}, 
+	    :shortline => {"mortgage_value" => "$800"
 	    },
 	  :rent_in_dollars => {
 	    :one_piece_owned => 25, 
